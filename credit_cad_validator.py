531b670d089fd8356f52a3c83046e3f3906cc6de
Phyu Thant Ko <phyuthantko28@gmail.com>
2023-10-10 12:41:11 +0800

    credit_card_finished

--- credit_cad_validator.py
+++ credit_cad_validator.py
@@ -11,9 +11,27 @@ print(card_number)
 
 #Step 2
 for x in card_number[::2]:
-    # sum_odd_digits += int(x)
+    sum_odd_digits += int(x)
     print(x)
 
+
+
 #Step 3
+for x in card_number[1::2]:
+    x = int(x) * 2
+    if x >= 10:
+        sum_even_digits += (1 + (x % 10))
+    else:
+        sum_even_digits += x;
+
+
+#Step 4
+total = sum_odd_digits + sum_even_digits
+
+#Step 5
+if total % 10 == 0:
+    print("VALID")
+else:
+    print("INVALID")
 
 
